1962e83139ade3c830d6cfd9408af0f6e773200f
Vizu <svncplaymc@gmail.com>
2026-04-06 08:10:42 +0700

    binding masih error tapi template udah

--- app/src/main/java/com/ubayadev/anmp_testgitw5/MainActivity.kt
+++ app/src/main/java/com/ubayadev/anmp_testgitw5/MainActivity.kt
@@ -7,8 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
